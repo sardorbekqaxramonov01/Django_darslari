b104f54f30e9577a4098e700355778fc3a123673
sardorbekqaxramonov01 <qaxramonovsardorbek03@gmail.com>
2023-07-30 22:35:32 +0500

    done

--- blog_site/models.py
+++ blog_site/models.py
@@ -11,5 +11,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     def get_absolute_url(self):
-        return reverse("contact", args=[str(self.id)])
+        return reverse("post_detail", args=[str(self.id)])
     
--- blog_site/urls.py
+++ blog_site/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import BlogListView,BlogCreateView,BlogContactView
+from .views import BlogListView,BlogCreateView,BlogDetailView,BlogupdateView,BlogDeleteView
 
 urlpatterns = [
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(),name="post_delete"),
+    path("post/<int:pk>/edit", BlogupdateView.as_view(),name="post_edit"),
     path("",BlogListView.as_view(), name="home"),
-    path("post/new/",BlogCreateView.as_view(),name="about"),
-    path("post/<int:pk>/", BlogContactView.as_view(),name="contact")
+    path("post/new/",BlogCreateView.as_view(),name="post_new"),
+    path("post/<int:pk>/", BlogDetailView.as_view(),name="post_detail")
 ]
