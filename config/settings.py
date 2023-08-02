31708b51564e86b7988ca4ff2e9035a5e720dbb6
sardorbekqaxramonov01 <qaxramonovsardorbek03@gmail.com>
2023-08-02 13:28:50 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'pages.apps.PagesConfig',
-    'blog_site.apps.BlogSiteConfig'
+    'blog_site.apps.BlogSiteConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('',include("pages.urls")),
-    path('',include("blog_site.urls"))
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/',include('accounts.urls')),
+    path('',include("blog_site.urls")),
 ]
